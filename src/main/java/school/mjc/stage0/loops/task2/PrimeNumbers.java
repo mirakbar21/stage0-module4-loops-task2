e09ba53748456aa22d8ccbb5613722ff7c6b27c4
Mirakbar Mirzokhidov <mirakbarmirzokhidov21@gmail.com>
2023-01-21 17:17:42 +0600

    upd: while loop

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -1,6 +1,22 @@
 package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
+    static boolean isPrime(int n){
+        if (n == 1 || n == 0) return false;
+        int i = 2;
+        while (i < n) {
+            if (n % i == 0) return false;
+            i++;
+        }
+        return true;
+    }
+
     public void printPrimeNumbers(int printToInclusive) {
+        int i = 2;
+
+        while (i <= printToInclusive) {
+            if (isPrime(i)) System.out.println(i);
+            i++;
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -2,6 +2,12 @@ package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
-
+        String word = "";
+        int i = 0;
+        while (i < chars.length){
+            word += chars[i];
+            i++;
+        }
+        System.out.print(word);
     }
 }
