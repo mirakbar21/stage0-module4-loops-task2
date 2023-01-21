2f7bec00ff942d83b7714d709e2e76beb097298c
Mirakbar Mirzokhidov <mirakbarmirzokhidov21@gmail.com>
2023-01-21 17:26:41 +0600

    upd: while loop

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -3,9 +3,13 @@ package school.mjc.stage0.loops.task2;
 public class PowerOfTwo {
     public void printPower(int power) {
         int i = 1, res = 1;
+
         if (power < 0) System.out.println("too much power");
-        if (power == 0) System.out.println("1");
         while (i <= power + 1){
+            if (power == 0) {
+                System.out.println("1");
+                break;
+            }
             System.out.println(res);
             res *= 2;
             ++i;
